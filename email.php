ddba7814c43805978036b549aec2361038c6ab33
Himanshux55 <98723354+Himanshux55@users.noreply.github.com>
2022-07-02 08:55:10 +0530

    Add files via upload

---
+++ email.php
@@ -0,0 +1,7 @@
+<?php
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'AKU GUNGRATE';
+$sender = 'From:  ▄︻̷ ┻ ═━一RAW_SONU <modyamrj@gmail.com>';
+
+$emailku = 'mangerrmt@gmail.com'; // GANTI EMAIL KAMU DISINI
+?>
